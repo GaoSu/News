feb72ebf932f699a8f228176bbf8c7fb9a7ad74c
GaoSuLiu <1025484542@qq.com>
2017-02-24 11:40:36 +0800

    创建项目，并改变背景颜色

---
+++ News/News/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by 刘 on 17/2/24.
+//  Copyright © 2017年 刘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.cyan
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
